e30142fb8d1dcf29e06850cb4f9db0dd5316b496
Fomagran <fomagran6@naver.com>
2021-05-03 11:44:21 +0900

    Top News 제스쳐 터치 범위 늘리기

--- SwiftUI MVVM Example/Views/NewsArticleView.swift
+++ SwiftUI MVVM Example/Views/NewsArticleView.swift
@@ -18,49 +18,52 @@ struct NewsArticleView: View {
     var body: some View {
         let screensize = UIScreen.main.bounds.size
         return  VStack(alignment: .leading) {
-            VStack(alignment:.leading) {
-                Text("Top News")
-                    .foregroundColor(Color.white)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(2)
-                    .gesture(DragGesture()
-                                .onChanged(self.onDragBegin)
-                                .onEnded(self.onDrageEnd)
-                    )
-                
-                Text("From 🍍News")
-                    .foregroundColor(Color.gray)
-                    .font(.body)
-                    .fontWeight(.bold)
-                    .padding(2)
-                
-                ScrollView {
-                    VStack{
-                        ForEach(self.newsArticles,id:\.title) {
-                            article in
-                            HStack {
-                                VStack(alignment: .leading){
-                                    Text(article.publication)
-                                        .foregroundColor(Color.white)
-                                        .font(.custom("Arial", size: 22))
-                                        .fontWeight(.bold)
-                                    
-                                    Text(article.title)
-                                        .foregroundColor(Color.white)
-                                        .font(.custom("Arial", size: 22))
-                                        .fontWeight(.bold)
-                                }
-                                Spacer()
-                                
-                                //URLImage 패키지 사용
-                                URLImage(url: URL(string: article.imageURL)!) { image in
-                                    image.resizable()
-                                }.frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                            }
-                        }
-                    }.frame(maxWidth:.infinity)
+            HStack {
+                VStack(alignment:.leading) {
+                    Text("Top News")
+                        .foregroundColor(Color.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(2)
+                    
+                    Text("From 🍍News")
+                        .foregroundColor(Color.gray)
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .padding(2)
                 }
+                Spacer()
+            }.padding().contentShape(Rectangle())
+            .gesture(DragGesture()
+                        .onChanged(self.onDragBegin)
+                        .onEnded(self.onDrageEnd)
+            )
+            
+            ScrollView {
+                VStack{
+                    ForEach(self.newsArticles,id:\.title) {
+                        article in
+                        HStack {
+                            VStack(alignment: .leading){
+                                Text(article.publication)
+                                    .foregroundColor(Color.white)
+                                    .font(.custom("Arial", size: 22))
+                                    .fontWeight(.bold)
+                                
+                                Text(article.title)
+                                    .foregroundColor(Color.white)
+                                    .font(.custom("Arial", size: 22))
+                                    .fontWeight(.bold)
+                            }
+                            Spacer()
+                            
+                            //URLImage 패키지 사용
+                            URLImage(url: URL(string: article.imageURL)!) { image in
+                                image.resizable()
+                            }.frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                }.frame(maxWidth:.infinity)
             }
         }.frame(width: screensize.width, height: screensize.height)
         .background(Color(red: 27/255, green: 28/255, blue: 30/255))
